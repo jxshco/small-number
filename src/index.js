56ad68b6de55cfd224aaa22531a4b06b2e3d0c2a
Josh Sturrock <jxshco@gmail.com>
2018-03-10 16:29:53 +0000

    Restructure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const smallNumber = (number, dec = 1, min = 1000) => {
+export default (number, dec = 1, min = 1000) => {
   if (number === undefined || number === null) throw new Error(`Error: ${number} is not a number`)
   if (isNaN(number)) throw new Error(`Error: ${number} is not a number`)
   if (number > Number.MAX_SAFE_INTEGER) throw new Error(`Error: Number is too big`)
@@ -8,5 +8,3 @@ const smallNumber = (number, dec = 1, min = 1000) => {
   let formatted = Number(str.substr(0, length % 3 !== 0 ? length % 3 : 3) + '.' + str.substr(length % 3 !== 0 ? length % 3 : 3)).toFixed(dec)
   return formatted + (length > 15 ? 'qd' : length > 12 ? 't' : length > 9 ? 'b' : length > 6 ? 'm' : length > 3 ? 'k' : '')
 }
-
-export default smallNumber
