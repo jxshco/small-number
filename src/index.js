90ed4b1161db850ee0930b0579a3aaf5deeb40d4
Joshua Sturrock <jxshco@gmail.com>
2019-10-08 20:48:47 +0100

    Package updates (#4)
    * Updated packages and linting
    
    * 1.2.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 export default (number, dec = 1, min = 1000) => {
   if (number === undefined || number === null) throw new Error(`Error: ${number} is not a number`)
   if (isNaN(number)) throw new Error(`Error: ${number} is not a number`)
-  if (number > Number.MAX_SAFE_INTEGER) throw new Error(`Error: Number is too big`)
+  if (number > Number.MAX_SAFE_INTEGER) throw new Error('Error: Number is too big')
   if (number < min) return String(number)
-  let str = String(number)
-  let length = str.length
-  let formatted = Number(str.substr(0, length % 3 !== 0 ? length % 3 : 3) + '.' + str.substr(length % 3 !== 0 ? length % 3 : 3)).toFixed(dec)
+  const str = String(number)
+  const length = str.length
+  const formatted = Number(str.substr(0, length % 3 !== 0 ? length % 3 : 3) + '.' + str.substr(length % 3 !== 0 ? length % 3 : 3)).toFixed(dec)
   return formatted + (length > 15 ? 'qd' : length > 12 ? 't' : length > 9 ? 'b' : length > 6 ? 'm' : length > 3 ? 'k' : '')
 }
